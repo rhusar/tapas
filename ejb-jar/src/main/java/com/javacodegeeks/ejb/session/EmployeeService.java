7b055469baa4483eeb522283254acd4c6559463c
Radoslav Husar <radosoft@gmail.com>
2010-12-23 20:00:57 +0100

    updated to gwt 2.1.1

--- ejb-jar/src/main/java/com/javacodegeeks/ejb/session/EmployeeService.java
+++ ejb-jar/src/main/java/com/javacodegeeks/ejb/session/EmployeeService.java
@@ -2,11 +2,15 @@ package com.javacodegeeks.ejb.session;
 
 import com.javacodegeeks.ejb.entity.Employee;
 
-
 public interface EmployeeService {
-	public Employee findEmployee(long employeeId);
-	public void saveEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
-	public void updateEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
-	public void saveOrUpdateEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
-	public void deleteEmployee(long employeeId) throws Exception;
+
+   public Employee findEmployee(long employeeId);
+
+   public void saveEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
+
+   public void updateEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
+
+   public void saveOrUpdateEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception;
+
+   public void deleteEmployee(long employeeId) throws Exception;
 }
--- ejb-jar/src/main/java/com/javacodegeeks/ejb/session/EmployeeServiceBean.java
+++ ejb-jar/src/main/java/com/javacodegeeks/ejb/session/EmployeeServiceBean.java
@@ -10,48 +10,48 @@ import com.javacodegeeks.ejb.entity.Employee;
 @Stateless(name = "employeeService")
 @Local(EmployeeService.class)
 public class EmployeeServiceBean implements EmployeeService {
-	@PersistenceContext
-	private EntityManager entityManager;
 
-	public void deleteEmployee(long employeeId) throws Exception {
-		entityManager.remove(entityManager.find(Employee.class, employeeId));
-	}
+   @PersistenceContext
+   private EntityManager entityManager;
 
-	public Employee findEmployee(long employeeId) {
-		return entityManager.find(Employee.class, employeeId);
-	}
+   @Override
+   public void deleteEmployee(long employeeId) throws Exception {
+      entityManager.remove(entityManager.find(Employee.class, employeeId));
+   }
 
-	public void saveEmployee(long employeeId, String name, String surname,
-			String jobDescription) throws Exception {
-		Employee emp = new Employee();
-		emp.setEmployeeId(employeeId);
-		emp.setEmployeeName(name);
-		emp.setEmployeeSurname(surname);
-		emp.setJob(jobDescription);
-		
-		entityManager.persist(emp);
+   public Employee findEmployee(long employeeId) {
+      return entityManager.find(Employee.class, employeeId);
+   }
 
-	}
+   public void saveEmployee(long employeeId, String name, String surname,
+           String jobDescription) throws Exception {
+      Employee emp = new Employee();
+      emp.setEmployeeId(employeeId);
+      emp.setEmployeeName(name);
+      emp.setEmployeeSurname(surname);
+      emp.setJob(jobDescription);
 
-	public void saveOrUpdateEmployee(long employeeId, String name,
-			String surname, String jobDescription) throws Exception {
-		Employee emp = new Employee();
-		emp.setEmployeeId(employeeId);
-		emp.setEmployeeName(name);
-		emp.setEmployeeSurname(surname);
-		emp.setJob(jobDescription);
-		
-		entityManager.merge(emp);
-	}
+      entityManager.persist(emp);
 
-	public void updateEmployee(long employeeId, String name, String surname,
-			String jobDescription) throws Exception {
-		Employee emp = entityManager.find(Employee.class, employeeId);
-		emp.setEmployeeName(name);
-		emp.setEmployeeSurname(surname);
-		emp.setJob(jobDescription);
-		
-		entityManager.merge(emp);
-	}
+   }
 
+   public void saveOrUpdateEmployee(long employeeId, String name,
+           String surname, String jobDescription) throws Exception {
+      Employee emp = new Employee();
+      emp.setEmployeeId(employeeId);
+      emp.setEmployeeName(name);
+      emp.setEmployeeSurname(surname);
+      emp.setJob(jobDescription);
+
+      entityManager.merge(emp);
+   }
+
+   public void updateEmployee(long employeeId, String name, String surname, String jobDescription) throws Exception {
+      Employee emp = entityManager.find(Employee.class, employeeId);
+      emp.setEmployeeName(name);
+      emp.setEmployeeSurname(surname);
+      emp.setJob(jobDescription);
+
+      entityManager.merge(emp);
+   }
 }
