84e19ca57b54dd1e880a4411ae2c548a3b0b2572
Radoslav Husar <radosoft@gmail.com>
2011-03-17 01:13:45 +0200

    forgotten GeneratedValue

--- tapas-ejb/src/main/java/com/radoslavhusar/tapas/ejb/entity/TaskTimeAllocation.java
+++ tapas-ejb/src/main/java/com/radoslavhusar/tapas/ejb/entity/TaskTimeAllocation.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -14,7 +16,7 @@ public class TaskTimeAllocation implements Serializable {
 
    private static final long serialVersionUID = 1L;
    @Id
-   @Column
+   @GeneratedValue(strategy = GenerationType.AUTO)
    private long id;
    @Column
    private double timeAllocation;
