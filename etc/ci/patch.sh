6c21716a709a85ca8243fdcc25f41996ff154882
Radoslav Husar <radosoft@gmail.com>
2011-04-06 17:24:58 +0200

    fixed diff in patch for production

--- etc/ci/patch.sh
+++ etc/ci/patch.sh
@@ -2,7 +2,7 @@
 
 echo "The diff is:"
 
-diff Application.gwt.xml ../../tapas-war/src/main/java/com/radoslavhusar/tapas/war
+diff Application.gwt.xml ../../tapas-war/src/main/java/com/radoslavhusar/tapas/war/Application.gwt.xml
 
 echo "Patching (overwrite)..."
 cp Application.gwt.xml ../../tapas-war/src/main/java/com/radoslavhusar/tapas/war
